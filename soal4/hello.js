b00c94ebedf13e8d2b110e3052150565be099fcb
ilmadinar <46672523+ilmadinar@users.noreply.github.com>
2019-03-22 14:51:10 +0700

    Create hello.js
    berisi hello.js

---
+++ soal4/hello.js
@@ -0,0 +1,8 @@
+main();
+function main(){
+var doc=document.createElement("textarea");
+	doc.style.width = "600";
+	doc.style.height = "300";
+	doc.innerHTML="Hello, Ilma Dinar Latiffa yang ber-NIM 10215099!"; 
+  document.body.append(doc);
+}
